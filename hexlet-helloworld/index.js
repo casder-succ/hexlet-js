8cf7be66335b08e5e2ff1e3da559dc0278021603
Casder <casderiopus@gmail.com>
2021-04-10 18:29:52 +0300

    rettier our files

--- hexlet-helloworld/index.js
+++ hexlet-helloworld/index.js
@@ -1,5 +1,5 @@
-import _ from "lodash"
+import _ from "lodash";
 
-console.log(_.last(['one', 'two']));
+console.log(_.last(["one", "two"]));
 
-console.log('Hello, World');
+console.log("Hello, World");
